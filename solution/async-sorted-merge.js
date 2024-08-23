c3c84297d1c3a7f29bd6040cb52052434c1cea6c
Jaime Liu <jaime.li.dev@gmail.com>
2024-08-23 14:58:39 -0600

    Solution sync and async fetch

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,8 +1,28 @@
 "use strict";
 
 // Print all entries, across all of the *async* sources, in chronological order.
+const MinHeap = require("../lib/minHeap");
 
-module.exports = (logSources, printer) => {
+module.exports = async (logSources, printer) => {
+  const minHeap = new MinHeap();
+
+  // Get each source asynchronously
+  await Promise.all(
+    logSources.map(async (source) => {
+      let log = await source.popAsync(); // Get the first log
+      while (log) { // Drain all logs from the source
+        minHeap.insertElement({ log }); 
+        log = await source.popAsync();
+      }
+    })
+  );
+
+  // Process the heap until all logs are printed
+  while (!minHeap.isEmpty()) {
+    const { log } = minHeap.popMin();
+    printer.print(log);
+  }
+
   return new Promise((resolve, reject) => {
     resolve(console.log("Async sort complete."));
   });
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,28 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+const MinHeap = require("../lib/minHeap");
+
 module.exports = (logSources, printer) => {
+  const minHeap = new MinHeap();
+
+  logSources.forEach((source) => {
+    const log = source.pop(); // Get first log
+    if (log) {
+      minHeap.insertElement({ log, source }); // Insert to Heap structure
+    }
+  });
+
+  while (!minHeap.isEmpty()) { 
+    const { log, source } = minHeap.popMin();
+    printer.print(log);
+
+    // Fetch the next log from the same source
+    const nextLog = source.pop();
+    if (nextLog) {
+      minHeap.insertElement({ log: nextLog, source }); // Get the next log from corresponding source
+    }
+  }
+
   return console.log("Sync sort complete.");
 };
